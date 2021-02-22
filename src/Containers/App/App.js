9639de278e87403284d5da22120b9fae86029c67
souiniaayoub <souiniaayoub@gmail.com>
2021-02-23 00:45:58 +0100

    finished

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,50 +2,7 @@ import React, { Component } from "react";
 import CardList from "../../Components/CardList/CardList";
 import "./App.css";
 import Topbar from "../../Components/Topbar/Topbar";
-const ImgsApi = "https://robohash.org/";
-
-const robots = [
-  {
-    name: "ayoub souinia",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub souinia",
-  },
-  {
-    name: "ayoub smt else",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub",
-  },
-  {
-    name: "ayoub souinia",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub souinia",
-  },
-  {
-    name: "ayoub smt else",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub",
-  },
-  {
-    name: "ayoub souinia",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub souinia",
-  },
-  {
-    name: "ayoub smt else",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub",
-  },
-  {
-    name: "ayoub souinia",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub souinia",
-  },
-  {
-    name: "ayoub smt else",
-    email: "email@gmail.com",
-    img: ImgsApi + "ayoub",
-  },
-];
+import robots from "../../robots";
 
 class App extends Component {
   state = {
@@ -53,7 +10,9 @@ class App extends Component {
   };
   onChangeHundler = (e) => {
     this.setState({
-      robots: robots.filter((rebot) => rebot.name.includes(e.target.value)),
+      robots: robots.filter((rebot) =>
+        rebot.name.toLowerCase().includes(e.target.value.toLowerCase())
+      ),
     });
   };
   render() {
---
+++ src/robots.js
@@ -0,0 +1,125 @@
+const ImgsApi = "https://robohash.org/";
+
+const robots = [
+  {
+    name: "Corion",
+    email: "Corion@mail.com",
+    img: ImgsApi + "Corion",
+  },
+
+  {
+    name: "Spirit",
+    email: "Spirit@mail.com",
+    img: ImgsApi + "Spirit",
+  },
+
+  {
+    name: "oye",
+    email: "oye@mail.com",
+    img: ImgsApi + "oye",
+  },
+
+  {
+    name: "ups",
+    email: "ups@mail.com",
+    img: ImgsApi + "ups",
+  },
+
+  {
+    name: "Spark",
+    email: "Spark",
+    img: ImgsApi + "Spark",
+  },
+
+  {
+    name: "Copper",
+    email: "Copper@mail.com",
+    img: ImgsApi + "Copper",
+  },
+
+  {
+    name: "Earl",
+    email: "Earl@mail.com",
+    img: ImgsApi + "Earl",
+  },
+
+  {
+    name: "Max",
+    email: "Max@mail.com",
+    img: ImgsApi + "Max",
+  },
+
+  {
+    name: "amib",
+    email: "amib@mail.com",
+    img: ImgsApi + "amib",
+  },
+
+  {
+    name: "Data",
+    email: "Data@mail.com",
+    img: ImgsApi + "Data",
+  },
+
+  {
+    name: "Azerty",
+    email: "Azerty@mail.com",
+    img: ImgsApi + "Azerty",
+  },
+
+  {
+    name: "Spark",
+    email: "Spark@mail.com",
+    img: ImgsApi + "Spark",
+  },
+
+  {
+    name: "Robert",
+    email: "Robert@mail.com",
+    img: ImgsApi + "Robert",
+  },
+
+  {
+    name: "omub",
+    email: "omub@mail.com",
+    img: ImgsApi + "omub",
+  },
+
+  {
+    name: "Ratcher",
+    email: "Ratcher@mail.com",
+    img: ImgsApi + "Ratcher",
+  },
+
+  {
+    name: "Bolt",
+    email: "Bolt@mail.com",
+    img: ImgsApi + "Bolt",
+  },
+
+  {
+    name: "Mecha",
+    email: "Mecha@mail.com",
+    img: ImgsApi + "Mecha",
+  },
+
+  {
+    name: "Tracker",
+    email: "Tracker@mail.com",
+    img: ImgsApi + "Tracker",
+  },
+
+  {
+    name: "Rubber",
+    email: "Rubber@mail.com",
+    img: ImgsApi + "Rubber",
+  },
+
+  {
+    name: "Scrappy",
+    email: "Scrappy@mail.com",
+    img: ImgsApi + "Scrappy",
+  },
+];
+
+export default robots;
